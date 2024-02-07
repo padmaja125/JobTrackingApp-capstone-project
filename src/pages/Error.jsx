b5175855d96f4c9161639ce2e791184ee7e55eb3
Padmaja <padhu.ramsh@gmail.com>
2024-02-07 12:29:22 -0500

    error page updated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/page_not_found.svg";
 
 const Error = () => {
+  const error = useRouteError;
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="page not found" />
+          <h3>Oops! Can't find now</h3>
+          <p>We can't able to find the page which you are looking for</p>
+          <Link to="/dashboard"> back home </Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      {" "}
-      <h1>Error Page</h1>
-      <Link to={"/"}>back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <img src={img} alt="page not found" />
+        <h3>Something went wrong </h3>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
